3ea8fe8e65905402480af6880796021a70dce5e2
Terry Cooper <terrycooper099@gmail.com>
2024-04-06 01:43:42 +0800

    add: bots.json

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ import uvicorn
 
 app = FastAPI()
 
+DEFAULT_MODEL = "GPT-4"
+BOTS_FILE = "bots.json"
 
-@app.post("/v1/chat/completions")
+
+@app.post("/openai/chat/completions")
 async def chat_completions(request: Request):
-
     response_template = {
         "id": "chatcmpl-123",
         "object": "chat.completion.chunk",
         "created": 123,
-        "model": "Claude-3-Opus",
+        "model": None,
         "system_fingerprint": "fp_123",
         "choices": [
             {
                 "index": 0,
-                "delta": {"role": "assistant", "content": ""},
+                "delta": {
+                    "role": "assistant",
+                    "content": ""
+                },
                 "logprobs": None,
                 "finish_reason": None
             }
@@ -27,9 +32,9 @@ async def chat_completions(request: Request):
     }
 
     request_body = await request.json()
-
     api_key = request.headers.get("Authorization").split(" ")[1]
-    bot_name = request_body["model"]
+    bot_map = json.load(open(BOTS_FILE, "r"))
+    response_template["model"] = bot_map.get(request_body["model"], DEFAULT_MODEL)
 
     def map_message(message):
         return {**message, "role": "bot" if message["role"] == "assistant" else message["role"]}
@@ -37,14 +42,20 @@ async def chat_completions(request: Request):
     messages = [fp.ProtocolMessage(**map_message(m)) for m in request_body["messages"]]
 
     async def generate():
-        async for chunk in fp.get_bot_response(messages=messages, bot_name=bot_name, api_key=api_key, temperature=0.2):
+        async for chunk in fp.get_bot_response(
+                messages=messages,
+                bot_name=response_template["model"],
+                api_key=api_key,
+                temperature=0.2
+        ):
             response = response_template.copy()
             response["choices"][0]["delta"]["content"] = chunk.text
             if not chunk.text:
                 response["choices"][0]["finish_reason"] = "stop"
-            yield f"data: {json.dumps(response)}\n\n"
+            yield f"data: {json.dumps(response)}\n"
 
     return StreamingResponse(generate(), media_type="text/event-stream")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8765)

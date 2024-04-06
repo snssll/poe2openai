86e13fb080797ac8bab48f28b27ceede805e4949
Terry Cooper <terrycooper099@gmail.com>
2024-04-06 13:15:11 +0800

    update: docs

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # poe2openai🚀
 
-This is a tool that 🛠️converts the POE developer API to the standard OpenAI API format.
-
+This is a tool that 🛠️seamlessly converts the POE developer API to the standard OpenAI API format.
 
 ## How to Use?🤔
 
-1. Prepare the local 🐳Docker environment;
-2. Clone this project to local;
-3. Enter the project directory and execute `docker build -t poe2openai .` to 🔧build the image;
-4. Execute `docker run -d --name poe2openai -p 8765:8765 poe2openai` to start the container;
+### One-Step Running
+
+```
+docker run -d --name poe2openai -p 8765:8765 1ynn/poe2openai:latest
+```
+
+### DIY
+
+1. Set up your local 🐳Docker environment;
+2. Clone this project to your local machine;
+3. Navigate to the project directory and run `docker build -t poe2openai .` to 🔧build the image;
+4. Run `docker run -d --name poe2openai -p 8765:8765 poe2openai` to start the container;
 5. Replace 🔄 `https://api.openai.com` in your application with `http://localhost:8765`;
 
 ## Q&A💬
 
-* How to get a POE API Key? 🔑 Please visit `https://poe.com/api_key`;
+* How can I get a POE API Key? 🔑 Please visit `https://poe.com/api_key`;
 * What are the 😄advantages? Unlimited access to Claude3 and GPT4 without worrying about regional restrictions;
 * What are the 😥disadvantages? Payment is required, and the first response is relatively slow;
 
--- main.py
+++ main.py
@@ -7,48 +7,60 @@ import uvicorn
 app = FastAPI()
 
 DEFAULT_MODEL = "GPT-4"
-BOTS_FILE = "bots.json"
+BOTS_LIST = json.load(open("bots.json", "r"))
+RESPONSE_TEMPLATE = {
+    "id": "chat-123",
+    "object": "chat.completion.chunk",
+    "created": 123,
+    "model": "",
+    "system_fingerprint": "fp_123",
+    "choices": [
+        {
+            "index": 0,
+            "delta": {
+                "role": "assistant",
+                "content": ""
+            },
+            "logprobs": None,
+            "finish_reason": ""
+        }
+    ]
+}
 
 
-@app.post("/openai/chat/completions")
-async def chat_completions(request: Request):
-    response_template = {
-        "id": "chatcmpl-123",
-        "object": "chat.completion.chunk",
-        "created": 123,
-        "model": None,
-        "system_fingerprint": "fp_123",
-        "choices": [
-            {
-                "index": 0,
-                "delta": {
-                    "role": "assistant",
-                    "content": ""
-                },
-                "logprobs": None,
-                "finish_reason": None
-            }
-        ]
-    }
-
+async def get_request_params(request: Request):
     request_body = await request.json()
-    api_key = request.headers.get("Authorization").split(" ")[1]
-    bot_map = json.load(open(BOTS_FILE, "r"))
-    response_template["model"] = bot_map.get(request_body["model"], DEFAULT_MODEL)
+    request_headers = request.headers
 
     def map_message(message):
         return {**message, "role": "bot" if message["role"] == "assistant" else message["role"]}
 
-    messages = [fp.ProtocolMessage(**map_message(m)) for m in request_body["messages"]]
+    return {
+        "api_key": request_headers.get("Authorization").split(" ")[1],
+        "model": BOTS_LIST.get(request_body["model"], DEFAULT_MODEL),
+        "max_tokens": request_body.get("max_tokens", 1024),
+        "messages": [fp.ProtocolMessage(**map_message(m)) for m in request_body["messages"]],
+        "temperature": request_body.get("temperature", 1),
+        "top_p": request_body.get("top_p", 1),
+        "frequency_penalty": request_body.get("frequency_penalty", 1),
+        "presence_penalty": request_body.get("presence_penalty", 1),
+        "stream": request_body.get("stream", False),
+    }
+
+
+@app.post("/openai/chat/completions")
+async def chat_completions(request: Request):
+    params = await get_request_params(request)
 
     async def generate():
         async for chunk in fp.get_bot_response(
-                messages=messages,
-                bot_name=response_template["model"],
-                api_key=api_key,
-                temperature=0.2
+                messages=params["messages"],
+                bot_name=params["model"],
+                api_key=params["api_key"],
+                temperature=params["temperature"],
         ):
-            response = response_template.copy()
+            response = RESPONSE_TEMPLATE.copy()
+            response["model"] = params["model"]
             response["choices"][0]["delta"]["content"] = chunk.text
             if not chunk.text:
                 response["choices"][0]["finish_reason"] = "stop"
